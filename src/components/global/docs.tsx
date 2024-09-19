20f492cc11b86eb1b0e1d2dc2d5f8f29afa067db
Bhavishya <121146811+bhavishya2324@users.noreply.github.com>
2024-09-19 22:39:41 +0530

    Update docs.tsx

--- src/components/global/docs.tsx
+++ src/components/global/docs.tsx
@@ -54,7 +54,7 @@ const Icons = {
   instagram: (props: IconProps) => (
     <svg
      
-     viewBox="0 0 24 24"
+     viewBox="0 0 32 32"
       fill="currentColor"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
@@ -145,11 +145,7 @@ const DATA = {
         url: "https://www.instagram.com/ultragleamindiapvt.ltd/",
         icon: Icons.instagram,
       },
-      email: {
-        name: "Send Email",
-        url: "#",
-        icon: Icons.email,
-      },
+     
     },
   },
 };
